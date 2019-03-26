07cb8797ee3e6ee9a0ddc0abb04242c1665847f7
Roshan <roshan@onezine.com>
2019-03-26 17:44:26 +0800

    added new methods

--- app/controllers/spree/enets_controller.rb
+++ app/controllers/spree/enets_controller.rb
@@ -4,52 +4,135 @@ require 'openssl'
 module Spree
   class EnetsController < StoreController
     before_action :load_order
-    before_action :payment_method, only: [:pay]
-    before_action :load_payment, only: %i[success cancel failure]
+    before_action :payment_method, only: [:index]
+    before_action :load_payment, only: [:cancel]
+    protect_from_forgery only: :index
 
-    def pay
-      txn_amt = @order.total
-      if (!txn_amt.nil?)
-        @txn_req = generate_payload(txn_amt, payment_method.preferred_umid)
-        @hmac = generate_signature(@txn_req, payment_method.preferred_secret_key)
-        @key_id = payment_method.preferred_public_key
+    def index
+        # payment_method = Spree::PaymentMethod.find_by(id: payment_method_id)
+        # success_url = payment_method.preferred_domain_name[0...-1].gsub("\n",'') + paysera_confirm_path(payment_method_id).gsub("\n",'')
+        # callback_url = payment_method.preferred_domain_name[0...-1].gsub("\n",'') + paysera_callback_path(payment_method_id).gsub("\n",'')
+        # cancel_url = payment_method.preferred_domain_name[0...-1].gsub("\n",'') + paysera_cancel_path(payment_method_id).gsub("\n",'')
 
-        # @payment = @order.payments.create!(amount: @order.total, payment_method: payment_method)
-        # @payment.started_processing!
-      end
+        # order = current_order || raise(ActiveRecord::RecordNotFound)
+        # amount = @order.total * 100
+        # payment_method.preferred_test_mode ? test_value = 1 : test_value = 0
+        # paytext_value = payment_method.preferred_message_text.present? ? payment_method.preferred_message_text : 'Payment'
 
-    rescue Exception => e
-      Rails.logger.error e.message
-      @payment.failure!
+        txn_amt = @order.total
 
-      flash[:error] = Spree.t(:enets_invalid)
-      redirect_to(:back) && return
+        if (!txn_amt.nil?)
+          @txn_req = generate_payload(txn_amt, payment_method.preferred_umid)
+          @hmac = generate_signature(@txn_req, payment_method.preferred_secret_key)
+          @key_id = payment_method.preferred_public_key
+
+          render 'index'
+
+          # @payment = @order.payments.create!(amount: @order.total, payment_method: payment_method)
+          # @payment.started_processing!
+        end
+
+        # begin
+        #   redirect_to url
+        # end
     end
 
-    def success
-      @payment.complete
-      @payment.order.next!
+    def callback
+        if params[:data].nil?
+            begin
+            redirect_to products_path
+            end
+            return
+        end
+        payment_method = Spree::PaymentMethod.find_by(id: params[:payment_method_id])
+        raise send_error("invalid payment method") if payment_method.type != "Spree::Gateway::Enets"
+        Spree::LogEntry.create({
+            source: payment_method,
+            details: params.to_yaml
+        })
+        response = parse(params)
+        if response[:projectid].to_i != payment_method.preferred_project_id
+            render plain: 'Error: project id does not match'
+            return
+        end
+        order = Spree::Order.find_by(number: response[:orderid])
 
-      if @payment.order.complete?
+        money = order.total * 100
+        if response[:payamount].to_i >= money.to_i
+            if response[:payamount].to_i > money.to_i
+                payment = order.payments.create!({
+                    source_type: 'Spree::Gateway::Enets',
+                    amount: response[:payamount].to_d/100,
+                    payment_method: payment_method
+                })
+                payment.complete
+                order.next
+
+                if order.payment_state == "paid"
+                    render plain: 'OK payment amount is greater than order total'
+                    return
+                else
+                    render plain: 'Error processing payment'
+                    return
+                end
+            else
+                payment = order.payments.create!({
+                    source_type: 'Spree::Gateway::Enets',
+                    amount: response[:payamount].to_d/100,
+                    payment_method: payment_method
+                })
+                payment.complete
+                order.next
+
+                if order.payment_state == "paid"
+                    render plain: 'OK'
+                    return
+                else
+                    render plain: 'Error processing payment'
+                    return
+                end
+            end
+        else
+            render plain: 'Error: bad order amount'
+            return
+        end
+
+
+    end
+
+    def confirm
+        payment_method = Spree::PaymentMethod.find_by(id: params[:payment_method_id])
+        raise send_error("invalid payment method") if payment_method.type != "Spree::Gateway::Enets"
+        if params[:data].nil?
+            begin
+            redirect_to products_path
+            end
+            return
+        end
+        response = parse(params)
+        raise send_error("'projectid' mismatch") if response[:projectid].to_i != payment_method.preferred_project_id
+        order = Spree::Order.find_by(number: response[:orderid])
+
+        if order.payment_state != "paid"
+            flash.alert = Spree.t(:payment_processing_failed)
+            begin
+            redirect_to cart_path
+            end
+            return
+        end
+
         flash.notice = Spree.t(:order_processed_successfully)
-        flash[:order_completed] = true
-        session[:order_id] = nil
-        redirect_to order_path(@payment.order)
-      else
-        redirect_to checkout_state_path(@payment.order.state)
-      end
+        begin
+        redirect_to user_root_path
+        end
+        return
     end
 
     def cancel
-      @payment.failure!
-      flash[:error] = Spree.t(:payment_processing_failed)
-      redirect_to checkout_state_path(@order.state)
-    end
-
-    def failure
-      @payment.failure!
-      flash[:error] = Spree.t(:payment_processing_failed)
-      redirect_to checkout_state_path(@order.state)
+        flash.notice = Spree.t(:order_canceled)
+        begin
+        redirect_to products_path
+        end
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
-  post "/enets/pay", to: "enets#pay"
-  get "/enets/failure", to: "enets#failure"
-  get "/enets/success", to: "enets#success"
-  get "/enets/cancel", to: "enets#cancel"
+  post '/enets', to: "enets#index", as: :enets_proceed
+  get '/enets/:pid/callback', to: "enets#callback", as: :enets_callback
+  get '/enets/:pid/confirm', to: "enets#confirm", as: :enets_confirm
+  get '/enets/:pid/cancel', to: "enets#cancel", as: :enets_cancel
 end

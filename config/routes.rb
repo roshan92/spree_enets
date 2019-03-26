071949322a3aa79ed399bc5f8d40d212c602bc9b
Roshan <roshan@onezine.com>
2019-03-26 15:50:24 +0800

    fixing respond_to err

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.draw do
-  post "/enets/pay", to: "enets#pay"
+  post "/enets/pay", to: "enets#pay", defaults: { format: 'js' }
   get "/enets/failure", to: "enets#failure"
   get "/enets/success", to: "enets#success"
   get "/enets/cancel", to: "enets#cancel"

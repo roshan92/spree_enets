0978beb13b5c3b6bcc03e2f703d6187455211eb2
Roshan <roshan@onezine.com>
2019-03-26 22:27:12 +0800

    fixed callback route to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
   post '/enets', to: "enets#index", as: :enets_proceed
-  get '/enets/:pid/callback', to: "enets#callback", as: :enets_callback
+  post '/enets/:pid/callback', to: "enets#callback", as: :enets_callback
   get '/enets/:pid/confirm', to: "enets#confirm", as: :enets_confirm
   get '/enets/:pid/cancel', to: "enets#cancel", as: :enets_cancel
 end

f90d71b6132aa7965015fed45087f9c4d811c083
Roshan <roshan@onezine.com>
2019-08-07 12:00:53 +0800

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Spree::Core::Engine.add_routes do
   post '/enets/:pid/confirm', to: "enets#confirm", as: :enets_confirm
   post '/enets/:pid/cancel', to: "enets#cancel", as: :enets_cancel
   post '/enets/:pid/server_callback', to: "enets#server_callback", as: :enets_server_callback
-  get '/enets/:pid/server_callback', to: "enets#server_callback", as: :enets_server_callback
+  get '/enets/:pid/server_callback', to: "enets#server_callback"
 end
